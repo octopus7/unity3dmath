ac0be3d6d65d6144dea57149dec0b8a1c2ab6004
octopus7 <octopus7net@gmail.com>
2018-06-08 12:31:18 +0900

    내적 외적

---
+++ math/Assets/scripts/Angle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Angle : MonoBehaviour {
+    public Transform a;
+    public Transform b;
+    public float angleFromDot;
+    public float angle;
+
+    void Update ()
+    {
+        float dot = Vector3.Dot(a.position.normalized, b.position.normalized);
+        angle = Vector3.Angle(a.position, b.position);
+        angleFromDot = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        Debug.Log(angle + " " + angleFromDot + " " + dot);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(Vector3.zero, a.position);
+        Gizmos.DrawRay(Vector3.zero, b.position);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(Vector3.zero, a.position.normalized);
+        Gizmos.color = Color.Lerp(Color.yellow, Color.red, 0.5f);
+        Gizmos.DrawRay(Vector3.zero, b.position.normalized);
+        Gizmos.color = Color.grey;
+    }
+}
---
+++ math/Assets/scripts/CrossProduct.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CrossProduct : MonoBehaviour {
+    public Transform a;
+    public Transform b;
+    public float angleFromDot;
+    public float angle;
+
+    Vector3 cross = Vector3.zero;
+
+    void Update ()
+    {
+        //float dot = Vector3.Dot(a.position.normalized, b.position.normalized);        
+        cross = Vector3.Cross(a.position, b.position);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(Vector3.zero, a.position);
+        Gizmos.DrawRay(Vector3.zero, b.position);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(Vector3.zero, a.position.normalized);
+        Gizmos.color = Color.Lerp(Color.yellow, Color.red, 0.5f);
+        Gizmos.DrawRay(Vector3.zero, b.position.normalized);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(Vector3.zero, cross);
+        Gizmos.color = Color.grey;
+    }
+}
